4e07f4c75c69dc33b93100881e195353ad88ba9c
seiya kato <sablier6969@yahoo.co.jp>
2021-01-30 18:12:50 +0900

    recommit

---
+++ pages/card.vue
@@ -0,0 +1,201 @@
+<template>
+    <div class="space1">
+        <v-container>
+            <div class="space2">
+                <v-container>
+                        <div>
+                            <h2 class="page-title">Thanks Card</h2>
+                            <p class="explan">ダウンロードボタンよりサンクスカードをお受け取りください。<br>ダウンロードが開始されない場合はカードを長押しして保存してください。</p>
+                            
+                        </div>
+                    
+                        <div>
+                            <div class="img-wrapper">
+                                <img src="../static/thankscard.png" class="thankscard">
+                            </div>
+                            <div class="button pat1">
+                                <a href="https://thanks-card.netlify.app/thankscard.png" class="link" download="thankscard.png">
+                                    <span class="link">
+                                        Download
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                    
+                </v-container>
+            </div>
+        </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+        }
+    }
+}
+</script>
+
+<style>
+.space1{
+    background: #eef2f3;  
+    width: 100%;
+    height:100vh;
+    position:relative;
+    padding:20px 0;
+}
+.space2{
+    font-size: 62.5%;
+    background-color: white;
+    width: 80%;
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translateY(-50%) translateX(-50%);
+}
+.img-wrapper{
+    width: 100%;
+    text-align: center;
+    margin: 0 auto;
+}
+.thankscard{
+    width: 20%;
+    margin-bottom:20px;
+}
+.page-title{
+    text-align: center;
+    font-family: 'Great Vibes', cursive;
+    color:rgb(240, 217, 159);
+    margin-bottom: 20px;
+    font-size:3rem;
+}
+.explan{
+    font-family: 'Noto Serif JP', serif;
+    font-size:1rem;
+}
+.button{
+    text-align: center;
+    margin: 0 auto;
+    color:rgb(240, 217, 159);
+    border:solid 1px rgb(240, 217, 159);
+    position: relative;
+    cursor: pointer;
+    width:100px;
+    height: 50px;
+    line-height: 50px;
+    transition: all 0.3s;
+    font-family: 'Source Sans Pro', sans-serif;
+    font-weight: 600;
+    font-style: italic;
+    font-size:15px;
+}
+.button:hover{
+    color:white;
+    border:solid 1px rgb(240, 217, 159);
+    background-color: rgb(240, 217, 159);
+}
+.button span{
+    z-index:3;
+    position: absolute;
+}
+.one{
+    position: absolute;
+    top:50%;
+    left: 50%;
+    transform:translate(-50%, -50%);
+    display: block;
+    width: 100%;
+    }
+.one:hover{
+    color:white;
+}
+.button span:hover{
+    z-index:3;
+    color:white;
+}
+.button::after{
+    content: '';
+    position: absolute;
+    transition: all 0.3s;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    transform: scale(0.1, 0.1);
+    z-index:1;
+}
+.button:hover::after {
+    background-color: rgb(240, 217, 159);
+    transform: scale(1, 1);
+    z-index:1;
+    color:white;
+}
+.pat1{
+    border:solid 1px rgb(240, 217, 159);
+    color:white;
+}
+.pat1:hover{
+    color:white;
+    border:solid 1px white;
+    background-color: rgb(240, 217, 159);
+}
+.pat1:hover::after {
+    background-color: rgb(240, 217, 159);
+    color:white;
+}
+.link{
+    color:rgb(240, 217, 159);
+    text-decoration: none;
+    display: block;
+    width: 100%;
+    position: absolute;
+    top:50%;
+    left: 50%;
+    transform:translate(-50%, -50%);
+    display: block;
+    width: 100%;
+    z-index:3;
+}
+.link:hover{
+    color:white;
+    text-decoration: none;
+    display: block;
+    width: 100%;
+}
+@media (min-width:1024px){
+    .space2{
+        padding-top:20px;
+    }
+    .explan{
+        width:70%;
+        margin: 0 auto;
+    }
+    .main{
+        margin-top:50px;
+    }
+    .sub{
+        display:none;
+    }
+}
+@media (min-width:768px) and ( max-width:1023px) {
+    .sub{
+        display:none;
+    }
+}
+@media (max-width: 767px){
+    .button{
+        height: 30px;
+        line-height: 30px;
+    }
+    .explan{
+        font-family: 'Noto Serif JP', serif;
+        font-size:0.8rem;
+    }
+    .thankscard{
+        margin-bottom:20px;
+        width:40%;
+    }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,209 @@
+<template>
+    <div class="wrap">
+        <div class="slide-wrapper">
+          <img src="../static/wed1.jpg" class="slideshow top">
+          <img src="../static/wed2.jpg" class="slideshow fadeout">
+          <img src="../static/wed3.jpg" class="slideshow fadeout">
+        </div>
+        <div class="qr-wrapper">
+            <p class="message1">Thank you<br>for coming</p>
+            <p class="message2">本日はお忙しい中ご出席いただき<br>誠にありがとうございました。</p>
+            <img src="../static/QR.png" class="qr">
+            <p class="message3">QRコードより新郎新婦からの<br>サンクスカードをお受け取り下さい。</p>
+        </div>
+        <div class="name">
+            <div class="names">Yuya & Yuri</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+          index: 0,
+        }
+    },
+    mounted() {
+        const images = document.getElementsByClassName('slideshow');
+        this.slideshow(images);
+        setInterval(() => {
+        this.index = this.index < images.length - 1 ? this.index + 1 : 0;
+        this.slideshow(images);
+        }, 8000);
+    },
+    methods: {
+        slideshow(images) {
+        const current = images[this.index];
+        const prev = images[this.index - 1] ? images[this.index - 1] : images[images.length - 1];
+        current.classList.add('fadein');
+        current.classList.remove('fadeout');
+        prev.classList.remove('fadein');
+        prev.classList.add('fadeout');
+        }
+    }
+}
+</script>
+
+<style>
+*, *::before, *::after {
+  box-sizing: border-box;
+  margin : 0;
+  padding : 0;
+}
+.fadein {
+    opacity: 1;
+    transition: opacity 4s;
+}
+.fadeout {
+    opacity: 0;
+    transition: opacity 4s;
+}
+
+.slideshow {
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+}
+.slide-wrapper{
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    z-index: 2;
+    overflow: hidden;
+}
+.wrap{
+    position: relative;
+    background-color: white;
+    overflow: hidden;
+    height:100vh;
+}
+.name{
+    position:absolute;
+    top:0;
+    left:0;
+    z-index:5;
+    font-family: 'Cormorant Garamond', serif;
+    font-weight:300;
+    text-transform: uppercase;
+    margin:0;
+}
+.names{
+    font-size: 90px;
+    color:#fffff0;
+    margin:0;
+    line-height:1;
+}
+.qr-wrapper{
+    position: absolute;
+    bottom:2%;
+    right:2%;
+    background-color: #E5BD54;
+    opacity:0.9;
+    z-index:4;
+    padding:30px;
+    border-radius: 10px;
+    text-align: center;
+}
+.message1{
+    color:white;
+    font-size: 50px;
+    margin-top: 10px;
+    font-family: 'Great Vibes', cursive;
+    line-height: 1;
+}
+.message2{
+    color:white;
+    margin-top: 16px;
+    font-family: 'Noto Serif JP', serif;
+    font-weight: 400;
+}
+.message3{
+    color:white;
+    margin-top: 16px;
+    font-family: 'Noto Serif JP', serif;
+    font-size:15px;
+    font-weight: 400;
+}
+.qr{
+      width:70%;
+      margin-top: 10px;
+    }
+@media (min-width:1024px){
+    .name{
+      bottom:0;
+    }
+}
+
+@media (min-width:768px) and (max-width: 1023px){
+    .qr-wrapper{
+      padding:10px;
+    }
+    .names{
+      font-size: 8vw;
+    }
+    .qr{
+      width:50%;
+    }
+    .message1{
+      font-size:30px;
+    }
+    .message2{
+      font-size:10px;
+    }
+    .message3{
+      font-size:10px;
+    }
+    .name{
+      position:absolute;
+      bottom:0;
+    }
+}
+
+@media (max-width: 767px){
+  .slideshow {
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+  }
+  .slide-wrapper{
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    z-index: 2;
+    overflow: hidden;
+  }
+  .top{
+    width: 100%;
+    height: 100vh;
+    object-position: 30% 70%;
+  }
+  .qr-wrapper{
+    padding:5px;
+  }
+  .qr{
+    width:50%;
+  }
+  .message1{
+    font-size:20px;
+    line-height: 1;
+  }
+  .message2{
+    font-size:10px;
+  }
+  .message3{
+    font-size:10px;
+  }
+  .name{
+    position:absolute;
+    top: 0;
+    left:0;
+  }
+.names{
+    font-size: 45px;
+    color:#fffff0;
+}
+}
+</style>
